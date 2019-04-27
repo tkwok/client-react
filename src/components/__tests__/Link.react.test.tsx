c8e74a4f4fc81817e783cfdb46995efcd0b20248
Tony Kwok <holypaladin07@gmail.com>
2019-04-27 16:01:28 -0700

    npm start dev working again

---
+++ src/components/__tests__/Link.react.test.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import Link from "../Link.react";
+import * as renderer from "react-test-renderer";
+
+test("Link changes the class when hovered", () => {
+  let component = renderer.create(React.createElement("h1", null, "qwe"));
+});

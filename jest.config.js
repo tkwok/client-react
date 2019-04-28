31b1fb7027993ef08e0fc50f863a4a2d4323ee47
Tony Kwok <holypaladin07@gmail.com>
2019-04-27 17:06:54 -0700

    added enzyme to work with jest and ts

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,9 @@ module.exports = {
   transform: {
     "^.+\\.tsx?$": "ts-jest"
   },
+  setupFiles: ["raf/polyfill"],
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  setupFilesAfterEnv: ["<rootDir>/setupTests.ts"],
+  snapshotSerializers: ["enzyme-to-json/serializer"]
 };

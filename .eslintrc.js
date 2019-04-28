e4941289733a33eac043e851c3221786e6b3a008
Tony Kwok <holypaladin07@gmail.com>
2019-04-28 00:50:18 -0700

    modified eslint on webpack config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,27 @@
 module.exports = {
   parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: "module"
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    jest: true,
+    commonjs: true
+  },
   extends: [
+    "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "react-app",
     "plugin:prettier/recommended"
   ],
   plugins: ["@typescript-eslint", "react"],
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: "module"
-  },
-  rules: {}
+  rules: {
+    semi: ["error", "always"],
+    quotes: ["error", "double"],
+    eqeqeq: "warn",
+    strict: "off"
+  }
 };

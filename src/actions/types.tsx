681d81d39d0a13bb1f705b7b7ed92cd49d897d1e
Tony Kwok <holypaladin07@gmail.com>
2019-05-17 23:54:06 -0700

    improve action type definition

--- src/actions/types.tsx
+++ src/actions/types.tsx
@@ -2,13 +2,24 @@ export const AUTH_USER = "auth_user";
 export const AUTH_ERROR = "auth_error";
 export const EDIT_USER = "edit_user";
 
-interface authPayload {
-    message: string
+/**
+ * Auth Payload Interface definition
+ * 
+ * @interface AuthPayload
+ */
+interface AuthPayload {
+    authenticated: boolean,
+    errorMessage: string
 };
 
+/**
+ * Auth Action Interfact
+ * 
+ * @interface AuthUserAction
+ */
 interface AuthUserAction {
     type: typeof AUTH_USER
-    payload: authPayload
-}
+    payload: AuthPayload
+};
 
 export type Types = AuthUserAction;
